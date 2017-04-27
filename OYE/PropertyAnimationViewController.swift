6d43988f30d37c9ec3d8a6cbbbee418aa6abb424
JT Ma <ma.jiangtao.86@gmail.com>
2017-04-27 10:50:08 +0800

    Add: Property Animator

--- OYE/PropertyAnimationViewController.swift
+++ OYE/PropertyAnimationViewController.swift
@@ -12,29 +12,49 @@ import UIKit
 class PropertyAnimationViewController: UIViewController {
     
     var circleCenter: CGPoint!
+    var circleAnimator: UIViewPropertyAnimator!
+    let animationDuration = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let circle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0))
-        circle.center = self.view.center
+        circle.center = view.center
         circle.layer.cornerRadius = 50.0
         circle.backgroundColor = UIColor.green
-        
         circle.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.dragCircle)))
+        view.addSubview(circle)
         
-        self.view.addSubview(circle)
+        circleAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeInOut)
     }
     
     func dragCircle(gesture: UIPanGestureRecognizer) {
         let target = gesture.view!
         
         switch gesture.state {
-        case .began:
+        case .began, .ended:
             circleCenter = target.center
+            
+            circleAnimator.stopAnimation(true)
+            
+            if gesture.state == .began {
+                circleAnimator.addAnimations {
+                    target.backgroundColor = .red
+                    target.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+                }
+            } else {
+                circleAnimator.addAnimations {
+                    target.backgroundColor = .green
+                    target.transform = CGAffineTransform.identity
+                }
+            }
+            
+            circleAnimator.startAnimation()
+            
         case .changed:
-            let translation = gesture.translation(in: self.view)
+            let translation = gesture.translation(in: view)
             target.center = CGPoint(x: circleCenter!.x + translation.x, y: circleCenter!.y + translation.y)
+            
         default: break
         }
     }
